0e72893c80664918eac2ba6b94739b571f32933a
Lilian N <izzopod@gmail.com>
2020-10-15 14:11:57 -0500

    remove test functions

--- index.js
+++ index.js
@@ -3,15 +3,6 @@ const http = require('http')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
 const sequelize = require('./sequelize')
-const seeder = require('./seeder')
-
-const init = async () => {
-  await sequelize.sync({ force: true })
-  await seeder.seedEverything()
-  const user = await sequelize.models.User.findByPk(1)
-}
-
-init()
 
 logger.info('Checking database connection...')
 sequelize.authenticate()
