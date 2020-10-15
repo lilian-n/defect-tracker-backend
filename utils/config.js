39dfb9feca5f17f9487ede2ce8b1db9a6818aaac
Lilian N <izzopod@gmail.com>
2020-10-15 14:10:38 -0500

    add auth0 config

--- utils/config.js
+++ utils/config.js
@@ -6,6 +6,9 @@ let DB_USERNAME = process.env.DB_USERNAME
 let DB_PASSWORD = process.env.DB_PASSWORD
 let LOGGING = true
 
+let AUTH0_AUDIENCE = process.env.AUTH0_AUDIENCE
+let AUTH0_ISSUER_URL = process.env.AUTH0_ISSUER_URL
+
 if (process.env.NODE_ENV === 'test') {
   DB_NAME = process.env.TEST_DB_NAME
   LOGGING = false
@@ -16,5 +19,7 @@ module.exports = {
   DB_NAME: DB_NAME,
   DB_USERNAME: DB_USERNAME,
   DB_PASSWORD: DB_PASSWORD,
-  LOGGING
+  LOGGING,
+  AUTH0_AUDIENCE,
+  AUTH0_ISSUER_URL
 }
