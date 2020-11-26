fb3755e1d468ee6d1a92448b75570c0400257dc9
Lilian N <izzopod@gmail.com>
2020-11-25 19:32:59 -0600

    add rbac

--- controllers/defects.js
+++ controllers/defects.js
@@ -6,25 +6,36 @@ const isLeadOrMember = (user) => {
   return (user.role === 'PROJECT LEAD') || (user.role === 'PROJECT MEMBER')
 }
 
-const isAssociatedWithDefect = async (user, defectId) => {
-  const defect = await models.Defect.findByPk(defectId)
-  const defectProject = defect.projectId
-  const identifier = defect.identifierId
-  const assignedDev = defect.assignedDevId
+const isAssociatedWithDefect = (user, defect) => {
+  if (user.role === 'ADMIN') {
+    return true
+  }
 
-  if (user.id === identifier) {
+  if (user.id === defect.identifierId) {
     return true
   }
-  if (user.id === assignedDev) {
+
+  if (user.id === defect.assignedDevId) {
     return true
   }
-  if ((user.role === 'PROJECT LEAD') && (user.projectId === defectProject)) {
+
+  if ((user.role === 'PROJECT LEAD') && (user.projectId === defect.projectId)) {
     return true
   }
+
+
+
   return false
 }
 
 defectsRouter.get('/', async (request, response) => {
+  const auth0Id = request.user.sub
+  const submitter = await models.User.findOne({ where: { auth0Id } })
+
+  if (submitter.role !== "ADMIN") {
+    return response.status(400).send("This user does not have permission to fetch defects.")
+  }
+
   const defects = await models.Defect.findAll({
     attributes: {
       exclude: ['createdAt', 'updatedAt']
@@ -42,6 +53,8 @@ defectsRouter.get('/', async (request, response) => {
 
 defectsRouter.get('/:id', async (request, response) => {
   const id = helpers.getIdParam(request)
+  const auth0Id = request.user.sub
+  const submitter = await models.User.findOne({ where: { auth0Id } })
 
   const defect = await models.Defect.findOne({
     where: { id },
@@ -57,91 +70,107 @@ defectsRouter.get('/:id', async (request, response) => {
     ]
   })
 
+  if (isLeadOrMember(submitter) && (submitter.projectId !== defect.projectId)) {
+    return response.status(400).send("This user does not have permission to view this defect.")
+  }
+
   response.json(defect)
+})
+
+defectsRouter.get('/defects-by-project/:id', async (request, response) => {
+  const projectId = helpers.getIdParam(request)
+  const auth0Id = request.user.sub
+  const submitter = await models.User.findOne({ where: { auth0Id } })
+
+  if ((submitter.role !== "ADMIN") && (submitter.projectId !== projectId)) {
+    return response.status(400).send("This user does not have permission to view these defects.")
+  }
+
+  const defects = await models.Defect.findAll({
+    where: {
+      projectId: projectId
+    }
+  })
+
+  response.json(defects)
 })
 
 defectsRouter.post('/', async (request, response) => {
   const auth0Id = request.user.sub
   const body = request.body
-
-  const submitter = await models.User.findOne({
-    where: {
-      auth0Id
-    }
-  })
+  const submitter = await models.User.findOne({ where: { auth0Id } })
 
   if (isLeadOrMember(submitter) && (submitter.projectId !== body.projectId)) {
-    response.status(400).send(`This user does not have permission to post defects to this project`)
-  } else {
-    const newDefect = {
-      summary: body.summary,
-      description: body.description || "",
-      dateIdentified: body.dateIdentified || new Date(),
-      priority: body.priority || "",
-      targetResDate: body.targetResDate || null,
-      progress: body.progress || "",
-      identifierId: submitter.id,
-      projectId: body.projectId,
-    }
-
-    const savedDefect = await models.Defect.create(newDefect)
-    response.json(savedDefect.toJSON())
+    return response.status(400).send(`This user does not have permission to post defects to this project`)
   }
+
+  const newDefect = {
+    summary: body.summary,
+    description: body.description || "",
+    dateIdentified: body.dateIdentified || new Date(),
+    priority: body.priority || "",
+    targetResDate: body.targetResDate || null,
+    progress: body.progress || "",
+    identifierId: submitter.id,
+    projectId: body.projectId,
+  }
+
+  const savedDefect = await models.Defect.create(newDefect)
+  response.json(savedDefect.toJSON())
 })
 
 defectsRouter.put('/:id', async (request, response, next) => {
   const auth0Id = request.user.sub
-  const id = helpers.getIdParam(request)
+  const defectId = helpers.getIdParam(request)
   const body = request.body
 
-  if (body.id === id) {
-    const submitter = await models.User.findOne({
-      where: {
-        auth0Id
-      }
-    })
-
-    if (await isAssociatedWithDefect(submitter, id)) {
-      const updateValues = {
-        summary: body.summary,
-        description: body.description,
-        status: body.status,
-        dateIdentified: body.dateIdentified,
-        priority: body.priority,
-        targetResDate: body.targetResDate,
-        actualResDate: body.actualResDate,
-        progress: body.progress,
-        resolutionSummary: body.resolutionSummary,
-        assignedDevId: body.assignedDevId,
-      }
-
-      models.Defect.setRevisingAuthor(submitter.id)
-      models.Defect.update(updateValues, {
-        returning: true,
-        where: { id }
-      })
-        .then(function ([rowsUpdate, [updatedDefect]]) {
-          response.json(updatedDefect.toJSON())
-        })
-        .catch(next)
-    } else {
-      response.status(400).send(`This user does not have permission to edit this defect.`)
-    }
-  } else {
-    response.status(400).send(`Bad request: id(${id}) does not match body id(${body.id})`)
+  if (body.id !== id) {
+    return response.status(400).send(`Bad request: id(${id}) does not match body id(${body.id})`)
   }
+
+  const defect = await models.Defect.findByPk(defectId)
+  const submitter = await models.User.findOne({ where: { auth0Id } })
+
+  if (!isAssociatedWithDefect(submitter, defect)) {
+    return response.status(400).send(`This user does not have permission to edit this defect.`)
+  }
+
+  const updateValues = {
+    summary: body.summary,
+    description: body.description,
+    status: body.status,
+    dateIdentified: body.dateIdentified,
+    priority: body.priority,
+    targetResDate: body.targetResDate,
+    actualResDate: body.actualResDate,
+    progress: body.progress,
+    resolutionSummary: body.resolutionSummary,
+    assignedDevId: body.assignedDevId,
+  }
+
+  models.Defect
+    .update(updateValues, {
+      returning: true,
+      where: { id },
+      individualHooks: true
+    })
+    .then(function ([rowsUpdate, [updatedDefect]]) {
+      response.json(updatedDefect.toJSON())
+    })
+    .catch(next)
 })
 
 defectsRouter.delete('/:id', async (request, response) => {
   const id = helpers.getIdParam(request)
-  const submitter = await models.User.findByPk(submitterId)
-  if (await isAssociatedWithDefect(submitter, id))
-    await models.Defect.destroy({
-      where: {
-        id: id
-      }
-    })
+  const auth0Id = request.user.sub
+  const submitter = await models.User.findOne({ where: { auth0Id } })
+  const defect = await models.Defect.findByPk(id)
 
+  if (!isAssociatedWithDefect(submitter, defect)) {
+    return response.status(400).send("This user does not have permission to delete this defect.")
+  }
+
+  await models.Defect.destroy({ where: { id: id } })
   response.status(204).end()
 })
 
