906ebc27bb2fd017ef7bd653e7c43b721bdd5e7e
Lilian N <izzopod@gmail.com>
2020-12-04 12:48:37 -0600

    test

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use('/api/users', usersRouter)
 app.use('/api/defects', defectsRouter)
 app.use('/api/comments', commentsRouter)
 
-app.use(middleware.unknownEndpoint)
+// app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
 module.exports = app
