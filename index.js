0177ef5badac8b6140556bb44dfee71676cd30f3
Lilian N <izzopod@gmail.com>
2020-12-04 13:02:20 -0600

    sync

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const logger = require('./utils/logger')
 const config = require('./utils/config')
 const sequelize = require('./sequelize')
 
+sequelize.sync({ force: true })
+
 logger.info('Checking database connection...')
 sequelize.authenticate()
   .then(() => {
