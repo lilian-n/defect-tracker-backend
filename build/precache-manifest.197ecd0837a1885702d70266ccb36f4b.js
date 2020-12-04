c3356f14c072236f9bd371c8b42cfecb22b4ef19
Lilian N <izzopod@gmail.com>
2020-12-04 12:56:51 -0600

    add build

---
+++ build/precache-manifest.197ecd0837a1885702d70266ccb36f4b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47ec55b85389ec1f9ba69240a155d40d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c18b7a2bf79ec66b7e25",
+    "url": "/static/css/main.0a7c0403.chunk.css"
+  },
+  {
+    "revision": "e9dd4112f1e3a106fb8f",
+    "url": "/static/js/2.6e36d32f.chunk.js"
+  },
+  {
+    "revision": "cdcefa1b4c2e3a7fa2bc562e4933edbc",
+    "url": "/static/js/2.6e36d32f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "503f7762a01ecf5e31b0",
+    "url": "/static/js/3.4add1d63.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/static/js/3.4add1d63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b2c7e0c99e794ad883b",
+    "url": "/static/js/4.99e3574e.chunk.js"
+  },
+  {
+    "revision": "0512cf1466087f5c44c5c61e4b32a001",
+    "url": "/static/js/4.99e3574e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15261f9b611cd6363967",
+    "url": "/static/js/5.968dd86c.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/static/js/5.968dd86c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c18b7a2bf79ec66b7e25",
+    "url": "/static/js/main.d47f10bc.chunk.js"
+  },
+  {
+    "revision": "52126ecb01b88ff18ad8",
+    "url": "/static/js/runtime-main.cd9fd8a4.js"
+  },
+  {
+    "revision": "148dbc25e2d521f02f6dbc9557ed829f",
+    "url": "/static/media/bug-icon.148dbc25.png"
+  }
+]);
