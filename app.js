5fda91af56356f260515a95c34a9832357d46e9d
Lilian N <izzopod@gmail.com>
2020-12-09 12:59:41 -0600

    change build file

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(middleware.errorHandler)
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')))
   app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build'))
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
   })
 } else {
   app.use(express.static('build'))
